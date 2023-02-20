68d7883eb3547bb9944a4a4bbc2baf2cf8d50a8b
minji <ireneeming@gmail.com>
2023-02-20 21:59:54 +0900

    ✨ : pomodoro timer

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_timer/screens/timer_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Pomodoro Timer App', home: TimerScreen());
+  }
+}
